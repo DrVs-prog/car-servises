8be19fcdb1e06f4e46ae7f35ac30c37513d6498d
DrVs-prog <korekovcev25@gmail.com>
2026-03-14 00:01:46 +0300

    CLOSE #10: Добавление настройки времени бездействия в App.config
    - Добавлен файл App.config с ключом InactivityTimeoutSeconds
    - Создан класс ConfigHelper для чтения настроек
    - Значение по умолчанию 30 секунд
    - Добавлена обработка ошибок чтения конфига

--- car_servises/ConfigHelper.cs
+++ car_servises/ConfigHelper.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace car_servises
 {
-    class ConfigHelper
+    public static class ConfigHelper
     {
+        public static int GetInactivityTimeout()
+        {
+            try
+            {
+                string value = ConfigurationManager.AppSettings["InactivityTimeoutSeconds"];
+
+                if (!string.IsNullOrEmpty(value) && int.TryParse(value, out int timeout))
+                {
+                    return timeout > 0 ? timeout : 30; // Не меньше 1 секунды
+                }
+            }
+            catch (Exception)
+            {
+                // Если ошибка чтения - используем значение по умолчанию
+            }
+
+            return 30; // Значение по умолчанию
+        }
     }
 }
